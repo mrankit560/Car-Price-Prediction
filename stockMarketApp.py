89b73d526af64599b366cb2a4f2b7f2fabbfaa1b
ANKIT <76831465+mrankit560@users.noreply.github.com>
2025-06-11 16:35:57 +0000

    create a simple web application stockMarketApp.py using strealit with simple features

--- stockMarketApp.py
+++ stockMarketApp.py
@@ -3,9 +3,19 @@ import yfinance as yf   #install yahoo finance stock market data of different co
 import datetime
 
 ticker_symbol = st.text_input("Enter the stock name", "AAPL")
-start_date = st.date_input("Start_date", datetime.date(2019, 7, 6))
-end_date = st.date_input("Start_date", datetime.date(2023, 7, 6))
+
+col1, col2 = st.columns(2)
+
+with col1:
+    start_date = st.date_input("Start_date", datetime.date(2019, 7, 6))
+with col2:
+    end_date = st.date_input("Start_date", datetime.date(2023, 7, 6))
 
 data = yf.download(ticker_symbol, start = start_date, end = end_date)
 
-data.write(data)
+st.write(data)
+
+st.line_chart(data["Close"])
+
+
+st.bar_chart(data["Volume"])
